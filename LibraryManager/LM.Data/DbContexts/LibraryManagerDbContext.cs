2acfc5375b2fc374ffcc49ab03866179fd4199fd
selinseidd <selinseid@abv.bg>
2021-12-10 21:52:01 +0200

    Initial commit

---
+++ LibraryManager/LM.Data/DbContexts/LibraryManagerDbContext.cs
@@ -0,0 +1,14 @@
+﻿using LM.Data.Entites;
+using Microsoft.EntityFrameworkCore;
+
+namespace LM.Data.DbContexts
+{
+    public class LibraryManagerDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Library> Library { get; set; }
+
+        public LibraryManagerDbContext(DbContextOptions<LibraryManagerDbContext> options) : base(options)
+        { }
+    }
+}
